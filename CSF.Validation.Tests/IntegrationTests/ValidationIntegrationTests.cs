ac55648e912beb379e9323fa95309626e2b3bd94
Craig Fowler <craig@craigfowler.me.uk>
2021-11-14 19:05:43 +0000

    Add an additional integration test

--- CSF.Validation.Tests/IntegrationTests/ValidationIntegrationTests.cs
+++ CSF.Validation.Tests/IntegrationTests/ValidationIntegrationTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using CSF.Validation.Rules;
 using NUnit.Framework;
 
 namespace CSF.Validation.IntegrationTests
@@ -28,6 +29,35 @@ namespace CSF.Validation.IntegrationTests
             var result = await validator.ValidateAsync(person);
 
             Assert.That(result.Passed, Is.True);
+        }
+
+        [Test,AutoMoqData]
+        public async Task ValidateAsyncShouldReturnFailureResultForSingleFailedRule([IntegrationTesting] IGetsValidator validatorFactory)
+        {
+            var validator = validatorFactory.GetValidator<Person>(typeof(PersonValidatorBuilder));
+
+            var person = new Person
+            {
+                Name = null,
+                Birthday = new DateTime(2000, 1, 1),
+                Pets = new[] {
+                    new Pet {
+                        Name = "Miffles",
+                        NumberOfLegs = 4,
+                        Type = "Cat"
+                    },
+                },
+            };
+
+            var result = await validator.ValidateAsync(person);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.Passed, Is.False, "Overall result");
+                Assert.That(result.RuleResults,
+                            Has.One.Matches<ValidationRuleResult>(r => r.Identifier.MemberName == "Name" && r.Outcome == RuleOutcome.Failed),
+                            "Correct failing rule");
+            });
         }
     }
 }
