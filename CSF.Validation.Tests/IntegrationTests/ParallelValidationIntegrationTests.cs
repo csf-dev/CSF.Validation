571797e42794fab08b0df0094bbcb999e45adb73
Craig Fowler <craig@craigfowler.me.uk>
2022-06-03 19:09:06 +0100

    Further increase grace period for AppVeyor
    It's OK as long as the grace period remains below 300ms.

--- CSF.Validation.Tests/IntegrationTests/ParallelValidationIntegrationTests.cs
+++ CSF.Validation.Tests/IntegrationTests/ParallelValidationIntegrationTests.cs
@@ -13,7 +13,7 @@ namespace CSF.Validation.IntegrationTests
         /// An amount of time in milliseconds that we allow the validator to run, beyond
         /// what we would expect in the best case scenario.
         /// </summary>
-        const int millisecondsGrace = 100;
+        const int millisecondsGrace = 150;
 
         [Test,AutoMoqData]
         public async Task ValidateAsyncShouldTakeApproximatelyTheCorrectTimeToRunAParallelRuleset([IntegrationTesting] IGetsValidator validatorFactory,
