d7ae548eb40b839f896d085dcad5a8690e28ee71
Craig Fowler <craig@craigfowler.me.uk>
2023-02-04 14:59:24 +0000

    WIP #33 - Another attempt to restore coverage

--- Tools/appveyor-tests.cmd
+++ Tools/appveyor-tests.cmd
@@ -1,2 +1,2 @@
 REM This command file is used with AppVeyor to ensure that it treats commandline params correctly
-dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput=TestResults/ --test-adapter-path:. --logger:nunit
+dotnet test --no-build /p:CollectCoverage=true /p:CoverletOutputFormat=\"opencover,json\" /p:CoverletOutput=\"TestResults/\" --test-adapter-path:. --logger:nunit
