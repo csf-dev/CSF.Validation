06cf1c61de42f59ee5b4de840644ff34d72b358b
Craig Fowler <craig@craigfowler.me.uk>
2023-02-05 19:27:28 +0000

    WIP #33 - Add coverage for recursive value setters

--- CSF.Validation.Tests/Manifest/RecursiveManifestValueTests.cs
+++ CSF.Validation.Tests/Manifest/RecursiveManifestValueTests.cs
@@ -10,13 +10,55 @@ namespace CSF.Validation.Manifest
     public class RecursiveManifestValueTests
     {
         [Test,AutoMoqData]
-        public void ToStringShouldReturnAStringWithTheTypeAndMemberName([Frozen] ManifestItem item)
+        public void ToStringShouldReturnAStringWithTheTypeAndMemberName(ManifestItem item)
         {
             Mock.Get(item).SetupGet(x => x.ValidatedType).Returns(typeof(string));
 
             var sut = new RecursiveManifestValue(item) { MemberName = "Foo" };
 
             Assert.That(() => sut.ToString(), Is.EqualTo("[RecursiveManifestValue: Type = String, MemberName = Foo]"));
+        }
+
+        [Test,AutoMoqData]
+        public void SetValidatedTypeShouldThrow(ManifestItem item)
+        {
+            var sut = new RecursiveManifestValue(item);
+            Assert.That(() => sut.ValidatedType = typeof(string), Throws.InstanceOf<NotSupportedException>())
+        }
+
+        [Test,AutoMoqData]
+        public void SetIdentityAccessorShouldThrow(ManifestItem item)
+        {
+            var sut = new RecursiveManifestValue(item);
+            Assert.That(() => sut.IdentityAccessor = x => "foo", Throws.InstanceOf<NotSupportedException>())
+        }
+
+        [Test,AutoMoqData]
+        public void SetCollectionItemValueShouldThrow(ManifestItem item)
+        {
+            var sut = new RecursiveManifestValue(item);
+            Assert.That(() => sut.CollectionItemValue = new ManifestCollectionItem(), Throws.InstanceOf<NotSupportedException>())
+        }
+
+        [Test,AutoMoqData]
+        public void SetChildrenShouldThrow(ManifestItem item)
+        {
+            var sut = new RecursiveManifestValue(item);
+            Assert.That(() => sut.Children = new List<ManifestValue>(), Throws.InstanceOf<NotSupportedException>())
+        }
+
+        [Test,AutoMoqData]
+        public void SetRulesShouldThrow(ManifestItem item)
+        {
+            var sut = new RecursiveManifestValue(item);
+            Assert.That(() => sut.Rules = new List<ManifestRule>(), Throws.InstanceOf<NotSupportedException>())
+        }
+
+        [Test,AutoMoqData]
+        public void SetAccessorExceptionBehaviourShouldThrow(ManifestItem item)
+        {
+            var sut = new RecursiveManifestValue(item);
+            Assert.That(() => sut.AccessorExceptionBehaviour = ValueAccessExceptionBehaviour.Throw, Throws.InstanceOf<NotSupportedException>())
         }
     }
 }
